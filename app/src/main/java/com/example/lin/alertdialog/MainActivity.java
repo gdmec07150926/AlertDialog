7df0840beb4fdc4ac4ccba3af6feff7d84a6084c
gdmec07150926 <z2222210@126.com>
2016-10-31 21:20:54 +0800

    Add files via upload

--- app/src/main/java/com/example/lin/alertdialog/MainActivity.java
+++ app/src/main/java/com/example/lin/alertdialog/MainActivity.java
@@ -1,13 +1,233 @@
 package com.example.lin.alertdialog;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class AlertDialogDome extends AppCompatActivity {
+    private AlertDialog dialog;
+    private AlertDialog.Builder builder;
+    private TextView tView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_alert_dialog_dome);
+        tView= (TextView) this.findViewById(R.id.textView1);
+        Button button1 = (Button) this.findViewById(R.id.button1);
+        Button button2 = (Button) this.findViewById(R.id.button2);
+        Button button3 = (Button) this.findViewById(R.id.button3);
+        Button button4 = (Button) this.findViewById(R.id.button4);
+        Button button5 = (Button) this.findViewById(R.id.button5);
+        Button button6 = (Button) this.findViewById(R.id.button6);
+        Button button7 = (Button) this.findViewById(R.id.button7);
+        View.OnClickListener listener=new View.OnClickListener(){
+            public void onClick(View v){
+                switch(v.getId()){
+                    case R.id.button1:
+                        dialog1();
+                        break;
+                    case R.id.button2:
+                        dialog2();
+                        break;
+                    case R.id.button3:
+                        dialog3();
+                        break;
+                    case R.id.button4:
+                        dialog4();
+                        break;
+                    case R.id.button5:
+                        dialog5();
+                        break;
+                    case R.id.button6:
+                        dialog6();
+                        break;
+                    case R.id.button7:
+                        dialog7();
+                        break;
+
+                }
+            }
+        };
+        button1.setOnClickListener(listener);
+        button2.setOnClickListener(listener);
+        button3.setOnClickListener(listener);
+        button4.setOnClickListener(listener);
+        button5.setOnClickListener(listener);
+        button6.setOnClickListener(listener);
+        button7.setOnClickListener(listener);
+    }
+
+    public void dialog1(){
+        dialog=new AlertDialog.Builder(this).create();
+        dialog.setTitle("提示");
+        dialog.setMessage("确认退出吗");
+        dialog.setIcon(android.R.drawable.alert_dark_frame);
+        //创建监听器
+        DialogInterface.OnClickListener listener=new DialogInterface.OnClickListener(){
+            public void onClick(DialogInterface dialog,int which){
+                if(which== DialogInterface.BUTTON_POSITIVE){
+                    dialog.dismiss();
+                    AlertDialogDome.this.finish();
+                }else if(which==DialogInterface.BUTTON_NEGATIVE){
+                    dialog.dismiss();
+                }
+            }
+        };
+        dialog.setButton(DialogInterface.BUTTON_POSITIVE,"确定",listener);
+        dialog.setButton(DialogInterface.BUTTON_NEGATIVE,"取消",listener);
+        dialog.show();
+    }
+    private void dialog2(){
+        dialog=new AlertDialog.Builder(this).create();
+        dialog.setTitle("调查");
+        dialog.setMessage("你平时忙吗");
+        dialog.setIcon(R.mipmap.ic_launcher);
+        DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String str="";
+                switch(which){
+                    case DialogInterface.BUTTON_POSITIVE:
+                        str="很忙";
+                        break;
+                    case DialogInterface.BUTTON_NEUTRAL:
+                        str="一般";
+                        break;
+                    case DialogInterface.BUTTON_NEGATIVE:
+                        str="很闲";
+                        break;
+                }
+                tView.setText(str);
+            }
+        };
+        dialog.setButton(DialogInterface.BUTTON_POSITIVE,"很忙吗",listener);
+        dialog.setButton(DialogInterface.BUTTON_NEUTRAL,"一般吗",listener);
+        dialog.setButton(DialogInterface.BUTTON_NEGATIVE,"很闲吗",listener);
+        dialog.show();
+    }
+    private void dialog3(){
+        dialog=new AlertDialog.Builder(this).create();
+        dialog.setTitle("调查");
+        dialog.setMessage("你平时有空吗");
+        final EditText edt=new EditText(this);
+        dialog.setView(edt);
+        DialogInterface.OnClickListener listener=new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                tView.setText(edt.getText().toString());
+            }
+        };
+        dialog.setButton(DialogInterface.BUTTON_POSITIVE,"确定",listener);
+        dialog.show();
+    }
+    private void dialog4(){
+        final String item[]=new String[]{"北京","上海","广州"};
+        final boolean sel[]=new boolean[item.length];
+        DialogInterface.OnMultiChoiceClickListener mListener=new DialogInterface.OnMultiChoiceClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                sel[which]=isChecked;
+            }
+        };
+        builder=new AlertDialog.Builder(this);
+        builder.setMultiChoiceItems(item,null,mListener);
+        dialog=builder.create();
+        dialog.setTitle("城市");
+        DialogInterface.OnClickListener listener=new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String str="你选择：";
+                for(int i=0;i<sel.length;i++){
+                    if(sel[i]){
+                        str+=item[i];
+                    }
+                }
+                tView.setText(str);
+            }
+        };
+        dialog.setButton(DialogInterface.BUTTON_POSITIVE,"确定",listener);
+        dialog.show();
+    }
+    private void dialog5(){
+        final String item[]=new String[]{"北京","上海","广州"};
+        final boolean sel[]=new boolean[item.length];
+        DialogInterface.OnClickListener sListener=new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                for(int i=0;i<sel.length;i++){
+                    if(i!=which){
+                        sel[i]=false;
+                    }else{
+                        sel[i]=true;
+                    }
+                }
+            }
+        };
+        builder = new AlertDialog.Builder(this);
+        builder.setSingleChoiceItems(item,-1,sListener);
+        dialog=builder.create();
+        dialog.setTitle("单选框");
+        DialogInterface.OnClickListener listener=new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String str="";
+                for(int i=0;i<sel.length;i++){
+                    if(sel[i]){
+                        str+=item[i];
+                    }
+                }
+                tView.setText(str);
+            }
+        };
+        dialog.setButton(DialogInterface.BUTTON_POSITIVE,"确定",listener);
+        dialog.show();
+    }
+    private void dialog6(){
+        final String item[]=new String[]{"北京","上海","广州"};
+        final boolean sel[]=new boolean[item.length];
+        DialogInterface.OnClickListener sListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String str="你选择";
+                str+=item[which];
+                tView.setText(str);
+            }
+        };
+        builder = new AlertDialog.Builder(this);
+        builder.setItems(item,sListener);
+        dialog = builder.create();
+        dialog.setTitle("数据列表");
+        DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        };
+        dialog.setButton(DialogInterface.BUTTON_POSITIVE,"取消",listener);
+        dialog.show();
+    }
+    private void dialog7(){
+        LayoutInflater inflater=getLayoutInflater();
+        View layout = inflater.inflate(R.layout.diy,null);
+        final EditText tEdit = (EditText) layout.findViewById(R.id.tEdit);
+        dialog = new AlertDialog.Builder(this).create();
+        dialog.setTitle("自定义");
+        dialog.setView(layout);
+        DialogInterface.OnClickListener listener=new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                tView.setText("输入的是："+tEdit.getText().toString());
+            }
+        };
+        dialog.setButton(DialogInterface.BUTTON_POSITIVE,"确定",listener);
+        dialog.setButton(DialogInterface.BUTTON_NEGATIVE,"取消",listener);
+        dialog.show();
     }
 }
